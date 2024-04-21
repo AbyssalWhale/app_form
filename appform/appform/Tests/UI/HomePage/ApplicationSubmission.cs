1289dc2c4803f2e3e1cb732ddb5cb7b50440c7d9
AbyssalWhale <53709071+AbyssalWhale@users.noreply.github.com>
2024-04-21 14:32:17 +0200

    Update ApplicationSubmission.cs
    moving data generation into setup

--- appform/appform/Tests/UI/HomePage/ApplicationSubmission.cs
+++ appform/appform/Tests/UI/HomePage/ApplicationSubmission.cs
@@ -5,9 +5,50 @@ namespace appform.Tests.UI.HomePage
 {
     public class ApplicationSubmission : TestsCoreFixtures
     {
+        private string FirstName;
+        private string LastName;
+        private string Email;
+        private string Password;
+        private string PasswordConfirmed;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            // Arrange
+            FirstName = Faker.Name.FirstName();
+            LastName = Faker.Name.LastName();
+            Email = Faker.Person.Email;
+            Password = Faker.Internet.Password();
+            PasswordConfirmed = Password;
+        }
+
         [Test]
         [Description("Automate the form submission with valid data and verify that the submission is successful and the data is correctly displayed on the success page.")]
         public async Task TestCase1()
+        {
+            // Act
+            await HomePage.InputFirstName(FirstName);
+            await HomePage.InputLastName(LastName);
+            await HomePage.InputEmail(Email);
+            await HomePage.InputPassword(Password);
+            await HomePage.InputConfirmPassword(PasswordConfirmed);
+            await HomePage.UnlockSlider();
+            var formSubmissionsPage = await HomePage.ClickSubmitButton();
+
+            // Assert 
+            await formSubmissionsPage.IsAtPage();
+            var isHeaderDisplayed = await formSubmissionsPage.IsHeaderDisplayed();
+            isHeaderDisplayed.ShouldBeEqualTo(true);
+
+            var firstNameActual = await formSubmissionsPage.GetName();
+            var emailActual = await formSubmissionsPage.GetEmail();
+            firstNameActual.ShouldBeEqualTo($"{FirstName} {LastName}");
+            emailActual.ShouldBeEqualTo(Email);
+        }
+
+        [Test]
+        [Description("Automate form submission with invalid data (e.g., mismatched passwords, invalid email format) and verify that the submission fails and appropriate validation messages are displayed.\r\n")]
+        public async Task TestCase2()
         {
             // Arrange
             var firstName = Faker.Name.FirstName();
