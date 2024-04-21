2501bf013d656c63b5faf82bd9ca302e6a672ab2
AbyssalWhale <53709071+AbyssalWhale@users.noreply.github.com>
2024-04-21 12:23:49 +0200

    adding project

---
+++ appform/appform/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace appform
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
